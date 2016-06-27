6c10c691caa16fa377a1b81c0384ca5b1d667854
zhangqq <z541833048@163.com>
2016-06-27 15:56:42 +0800

    add files

---
+++ ZQWebViewHeader/ZQWebViewHeader.h
@@ -0,0 +1,13 @@
+//
+//  ZQWebViewHeader.h
+//  ZQWebViewHeader
+//
+//  Created by zhang on 16/6/27.
+//  Copyright © 2016年 zhangqq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQWebViewHeader : UIWebView
+
+@end
---
+++ ZQWebViewHeader/ZQWebViewHeader.m
@@ -0,0 +1,21 @@
+//
+//  ZQWebViewHeader.m
+//  ZQWebViewHeader
+//
+//  Created by zhang on 16/6/27.
+//  Copyright © 2016年 zhangqq.com. All rights reserved.
+//
+
+#import "ZQWebViewHeader.h"
+
+@implementation ZQWebViewHeader
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
