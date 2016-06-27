972c540d57ff3502818bf26bb2f10c7edb55e037
zhangqq <z541833048@163.com>
2016-06-27 11:35:04 +0800

    Initial Commit

---
+++ ZQWebViewHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQWebViewHeader
+//
+//  Created by zhang on 16/6/27.
+//  Copyright © 2016年 zhangqq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
